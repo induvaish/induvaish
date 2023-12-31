b91f542248a67157c24f110ffc11dafc433bcefd
induvaish <139248706+induvaish@users.noreply.github.com>
2023-12-31 11:08:21 +0530

    Add files via upload
    Starbucks, the renowned coffee company, has had various projects over the years. Are you referring to a specific project within Starbucks, like a new product launch, sustainability initiative

---
+++ starbucks/script.js
@@ -0,0 +1,11 @@
+
+function imgSlider(anything)
+         {
+          document.querySelector('.Starbucks').src=anything;
+         }
+
+         function changecirclecolor(color)
+         {
+          const circle= document.querySelector('.circle');
+          circle.style.background=color;
+         }
